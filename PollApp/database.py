9e26893f050270ca4a37e8cfe37869bc065e1a18
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2026-02-08 22:54:44 +0700

    create public db schema

--- PollApp/database.py
+++ PollApp/database.py
@@ -8,7 +8,9 @@ from sqlmodel import create_engine, SQLModel, Session
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Halilintar99@localhost/PollApplicationDatabase'
 SQLALCHEMY_DATABASE_URL = 'postgresql://scoring_app_wentwhich:77c0c98b804ac621263126cf1325e9ab46b9a709@1-59hp.h.filess.io:5434/scoring_app_wentwhich'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
+    "options": "-csearch_path=public"
+})
 
 
 def create_db_and_tables():
